bb882139f4239d50c8e85abe48d1c8b730699452
Lin <linchaopai@gmail.com>
2025-06-18 22:11:33 +0800

    format the correlation coefficient into APA format (remove the leading 0)

--- R/cor_table.R
+++ R/cor_table.R
@@ -47,14 +47,17 @@
 #'   digits_corr = 2
 #' )
 cor_table <- function(data, vars,
-                            digits_stat     = 2,
-                            digits_corr     = 3,
-                            stats_position  = c("left", "bottom")) {
-  # match new option names
+                      digits_stat    = 2,
+                      digits_corr    = 3,
+                      stats_position = c("left", "bottom")) {
+
   stats_position <- match.arg(stats_position)
 
-  # for tidyselect
-  library(dplyr)
+  ## correlation formatter (APA 7) ----------------------------------------
+  fmt_r <- function(x, digits) {
+    sub("^(-?)0\\.", "\\1.", sprintf(paste0("%.", digits, "f"), x))
+  }
+  ## ----------------------------------------------------------------------
 
   # select and name variables
   data_sel  <- select(data, {{ vars }})
@@ -87,52 +90,46 @@ cor_table <- function(data, vars,
     }
   }
 
-  # format into character matrix
+  # build character matrix
   formatted_corr <- matrix("", n, n)
   for (i in seq_len(n)) {
     for (j in seq_len(n)) {
       if (i > j) {
-        r <- sprintf(paste0("%.", digits_corr, "f"), corr_matrix[i, j])
+        r <- fmt_r(corr_matrix[i, j], digits_corr)  # CHANGED
         s <- get_stars(p_matrix[i, j])
         formatted_corr[i, j] <- paste0(r, s)
       } else if (i == j) {
-        formatted_corr[i, j] <- "\u2013"   # diagonal
+        formatted_corr[i, j] <- "\u2013"             # diagonal
       }
     }
   }
 
-  # output
+  # assemble final table
   if (stats_position == "left") {
-    # descriptive stats on left
     corr_df <- as.data.frame(formatted_corr, stringsAsFactors = FALSE)
     colnames(corr_df) <- as.character(seq_len(n))
 
     final_df <- data.frame(
       Variable = paste0(seq_len(n), ". ", var_names),
-      M        = sprintf(paste0("%.", digits_stat, "f"), means),
-      SD       = sprintf(paste0("%.", digits_stat, "f"), sds),
+      M  = sprintf(paste0("%.", digits_stat, "f"), means),  # zero kept
+      SD = sprintf(paste0("%.", digits_stat, "f"), sds),    # zero kept
       corr_df,
-      check.names = FALSE,
-      stringsAsFactors = FALSE
+      check.names = FALSE, stringsAsFactors = FALSE
     )
 
-  } else {
-    # stats on bottom
-    # build matrix of labels + correlations
+  } else {  # stats on bottom
     tbl <- matrix("", n + 2, n + 1)
     colnames(tbl) <- c("Variable", as.character(seq_len(n)))
 
-    # variable rows
     for (i in seq_len(n)) {
       tbl[i, 1] <- paste0(i, ". ", var_names[i])
-      tbl[i, 2:(n+1)] <- formatted_corr[i, ]
+      tbl[i, 2:(n + 1)] <- formatted_corr[i, ]
     }
-    # add M and SD rows
-    tbl[n+1, 1] <- "M"
-    tbl[n+2, 1] <- "SD"
+    tbl[n + 1, 1] <- "M"
+    tbl[n + 2, 1] <- "SD"
     for (j in seq_len(n)) {
-      tbl[n+1, j+1] <- sprintf(paste0("%.", digits_stat, "f"), means[j])
-      tbl[n+2, j+1] <- sprintf(paste0("%.", digits_stat, "f"), sds[j])
+      tbl[n + 1, j + 1] <- sprintf(paste0("%.", digits_stat, "f"), means[j])
+      tbl[n + 2, j + 1] <- sprintf(paste0("%.", digits_stat, "f"), sds[j])
     }
 
     final_df <- as.data.frame(tbl, stringsAsFactors = FALSE, check.names = FALSE)
